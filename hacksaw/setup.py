f7c96b34b1b671bbec5d958b547804c8ff826a2c
Graham Ashton <graham@effectif.com>
2004-10-12 11:52:23 +0000

    - new file.

---
+++ hacksaw/setup.py
@@ -0,0 +1,19 @@
+# $Id$
+# (C) Cmed Ltd, 2004
+
+
+from distutils.core import setup
+
+
+setup(name="hacksaw",
+      version="0.1.0",
+      author="Cmed Technology",
+      author_email="developers@cmedltd.com",
+      url="http://hacksaw.sourceforge.net/",
+      download_url="http://hacksaw.sourceforge.net/download.html",
+      description="Log file monitoring and alerting",
+      package_dir={"": "src"},
+      packages=["hacksaw", "hacksaw.proc"],
+      scripts=["src/processlogs.py", "src/retrievelogs.py"],
+      data_files=[("etc", ["etc/hacksaw.conf"])]
+      )
