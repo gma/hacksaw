66a5e5e5d0bf03dec3c770fc7a48587320e833b5
Andrew Hamilton <andhamilton@users.sourceforge.net>
2004-10-13 14:29:02 +0000

    - Made more quiet. - Don't copy empty logs.

--- hacksaw/scripts/push-logs.sh
+++ hacksaw/scripts/push-logs.sh
@@ -6,13 +6,13 @@
 # (C) Cmed Ltd, 2004
 
 
-SYSLOG=syslog
+SYSLOG=syslog  # a pattern that matches process name, not the actual name
 
 
 function usage()
 {
-    echo "Usage: $(basename $0) [-u username] [-d remotedir] [-l (logfile|logdir)] hostname"\
-	1>&2
+    echo "Usage: $(basename $0) [-u username] [-d remotedir]" \
+	"[-l (logfile|logdir)] hostname" 1>&2
 }
 
 
@@ -21,14 +21,20 @@ function push_log()
     local log_file=$1
     local remote_host=$2
     local login=$3
-    local directory=$4
-    timestamp=$(date -u +%y%m%d-%H%M%S)
-    log_name=$(hostname)_$(basename $log_file)_$timestamp
+    local remotedir=$4
+    if [ ! -f $log_file ]; then
+	return
+    fi
+    timestamp=$(date -u +%Y%m%d-%H%M%S)
+    log_name=$(hostname)-$(basename $log_file)-$timestamp
     temp_file=$(tempfile)
     mv $log_file $temp_file
     pkill -HUP -f $SYSLOG
-    scp $temp_file $login@$remote_host:$directory/$log_name.tmp
-    ssh $login@$remote_host "mv $directory/$log_name.tmp $directory/$log_name"
+    if [ -s $temp_file ]; then
+	scp -Bq $temp_file $login@$remote_host:$remotedir/$log_name.copy
+	ssh $login@$remote_host \
+            "mv $remotedir/$log_name.copy $remotedir/$log_name"
+    fi
     rm $temp_file
 }
 
@@ -37,7 +43,7 @@ set -e
 
 log_path=$(pwd)
 login=root
-directory=/var/spool
+remotedir=/var/spool
 # Process command line args
 while getopts ":u:d:l:" opt; do
     case $opt in
@@ -45,7 +51,7 @@ while getopts ":u:d:l:" opt; do
 	    ;;
 	u ) login=$OPTARG
 	    ;;
-	d ) directory=$OPTARG
+	d ) remotedir=$OPTARG
 	    ;;
     esac
 done
@@ -58,8 +64,8 @@ remote_host=$1
 
 if [ -d $log_path ]; then
     for log_file in $(ls $log_path); do
-        push_log $log_path/$log_file $remote_host $login $directory
+        push_log $log_path/$log_file $remote_host $login $remotedir
     done
 else
-    push_log $log_path $remote_host $login $directory
+    push_log $log_path $remote_host $login $remotedir
 fi
