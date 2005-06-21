bd52fa773e3d19c28f46f7bd9fd1b9769b8869de
Graham Ashton <graham@effectif.com>
2005-06-21 12:34:42 +0000

    - latest changes; can now create a full packet, working towards UDP delivery.

--- hacksaw/src/hacksaw/proc/remotesyslog.py
+++ hacksaw/src/hacksaw/proc/remotesyslog.py
@@ -1,8 +1,28 @@
+# Copyright (C) 2005 Graham Ashton <ashtong@users.sourceforge.net>
 # $Id$
-# (C) Cmed Ltd, 2004
 
 
+"""send messages to a remote syslog server
+
+This module provides classes for constructing UDP packets that can be
+sent to a remote syslog server. It attempts to follow the protocol
+defined in RFC 3164.
+
+The classes and attributes are named according to the terminology in
+the RFC. In the absence of full documentation for this module you
+should be able to read the RFC to work out how the code is
+structured. If you are interested in sending syslog messages to a
+remote server you shouldn't need to look any further than the Logger
+class.
+
+For more information see http://hacksaw.sourceforge.net/remotesyslog/
+
+"""
+
+
+import os
 import socket
+import sys
 import time
 
 import hacksaw.lib
@@ -67,14 +87,83 @@ class HeaderPart(object):
     hostname = property(_get_hostname, _set_hostname)
 
 
-class SyslogMessage(object):
+class MsgPart(object):
 
-    def __init__(self, pri, header):
-        self.pri = pri
-        self.header = header
+    MAX_TAG_LEN = 32
+
+    def __init__(self, tag=None, content=""):
+        self.tag = tag
+        self.content = content
 
     def __str__(self):
-        return "%s%s" % (self.pri, self.header)
+        return self.tag + self.content
+
+    def _get_tag(self):
+        return self._tag
+
+    def _set_tag(self, value):
+        if value is None:
+            value = sys.argv[0]
+        self._tag = value[:self.MAX_TAG_LEN]
+
+    tag = property(_get_tag, _set_tag)
+
+    def _get_content(self):
+        return self._content
+
+    def _prepend_seperator(self, value):
+        try:
+            first_char = value[0]
+        except IndexError:
+            pass
+        else:
+            if first_char.isalnum():
+                value = ": " + value
+        return value
+
+    def _set_content(self, value):
+        value = self._prepend_seperator(value)
+        self._content = value
+
+    content = property(_get_content, _set_content)
+
+    def include_pid(self):
+        self.content = "[%d]" % os.getpid() + self.content
+
+
+class SyslogMessage(object):
+
+    MAX_LEN = 1024
+
+    def __init__(self, pri, header, msg):
+        self.pri = pri
+        self.header = header
+        self.msg = msg
+
+    def __str__(self):
+        message = "%s%s %s" % (self.pri, self.header, self.msg)
+        return message[:self.MAX_LEN]
+
+
+class Logger(object):
+
+    PORT = 514
+
+    def __init__(self):
+        self._sockets = []
+
+    def add_host(self, hostname):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.connect((hostname, self.PORT))
+        self._sockets.append(sock)
+
+    def log(self, facility, level, text):
+        pri = PriPart(facility, level)
+        header = HeaderPart()
+        msg = MsgPart(content=text)
+        data = str(SyslogMessage(pri, header, msg))
+        for sock in self._sockets:
+            sock.send(data)
     
 
 class Processor(hacksaw.lib.Processor):
--- hacksaw/src/hacksaw/proc/remotesyslog_test.py
+++ hacksaw/src/hacksaw/proc/remotesyslog_test.py
@@ -1,11 +1,15 @@
+# Copyright (C) 2005 Graham Ashton <ashtong@users.sourceforge.net>
 # $Id$
-# (C) Cmed Ltd, 2005
 
 
+import os
 import socket
+import sys
 import syslog
 import time
 import unittest
+
+from pmock import *
 
 import hacksaw.proc.remotesyslog
 
@@ -23,8 +27,8 @@ DEFAULT_TIMESTAMP = "Jun  7 09:00:00"
 DEFAULT_HOSTNAME = "myhost"
 DEFAULT_HEADER = "%s %s" % (DEFAULT_TIMESTAMP, DEFAULT_HOSTNAME)
 
-    
-class HeaderPartTest(unittest.TestCase):
+
+class MockHeaderTest(unittest.TestCase):
 
     def mock_localtime(self):
         return (2005, 6, 7, 9, 0, 0, 1, 158, 1)  # see DEFAULT_TIMESTAMP
@@ -32,60 +36,47 @@ class HeaderPartTest(unittest.TestCase):
     def mock_gethostname(self):
         return "myhost"
 
-    def setup_mock(self):
+    def setUp(self):
         self.real_localtime = time.localtime
         time.localtime = self.mock_localtime
         self.real_gethostname = socket.gethostname
         socket.gethostname = self.mock_gethostname
 
-    def reset_mock(self):
+    def tearDown(self):
         time.localtime = self.real_localtime
         socket.gethostname = self.real_gethostname
 
+    
+class HeaderPartTest(MockHeaderTest):
+
     def test_automatic_timestamp(self):
         """Check HEADER is automatically calculated if not set"""
-        self.setup_mock()
-        try:
-            header = hacksaw.proc.remotesyslog.HeaderPart()
-            self.assertEqual(str(header), DEFAULT_HEADER)
-        finally:
-            self.reset_mock()
+        header = hacksaw.proc.remotesyslog.HeaderPart()
+        self.assertEqual(str(header),
+                         " ".join((DEFAULT_TIMESTAMP, DEFAULT_HOSTNAME)))
 
     def test_incorrect_characters_disallowed(self):
         """Check only valid characters are used in the HEADER"""
         # Only allowed characters are ABNF VCHAR values and space.
         # Basically, if ord() returns between 32 and 126 inclusive
         # it's okay.
-        self.setup_mock()
-        try:
-            bad_char = u"\x1f"  # printable, ord() returns 31
-            header = hacksaw.proc.remotesyslog.HeaderPart()
-            header.timestamp = header.timestamp[:-1] + bad_char
-            self.assertEqual(str(header), DEFAULT_HEADER)
-        finally:
-            self.reset_mock()
+        bad_char = u"\x1f"  # printable, ord() returns 31
+        header = hacksaw.proc.remotesyslog.HeaderPart()
+        header.timestamp = header.timestamp[:-1] + bad_char
+        self.assertEqual(str(header),
+                         " ".join((DEFAULT_TIMESTAMP, DEFAULT_HOSTNAME)))
 
     def test_set_timestamp_manually(self):
         """Check it is possible to set the timestamp in HEADER manually"""
-        self.setup_mock()
-        try:
-            timestamp = "Jan 31 18:12:34"
-            header = hacksaw.proc.remotesyslog.HeaderPart(timestamp=timestamp)
-            self.assertEqual(str(header),
-                             "%s %s" % (timestamp, DEFAULT_HOSTNAME))
-        finally:
-            self.reset_mock()
+        timestamp = "Jan 31 18:12:34"
+        header = hacksaw.proc.remotesyslog.HeaderPart(timestamp=timestamp)
+        self.assertEqual(str(header), "%s %s" % (timestamp, DEFAULT_HOSTNAME))
 
     def test_set_hostname_manually(self):
         """Check it is possible to set the hostname in HEADER manually"""
-        self.setup_mock()
-        try:
-            hostname = "otherhost"
-            header = hacksaw.proc.remotesyslog.HeaderPart(hostname=hostname)
-            self.assertEqual(str(header),
-                             "%s %s" % (DEFAULT_TIMESTAMP, hostname))
-        finally:
-            self.reset_mock()
+        hostname = "otherhost"
+        header = hacksaw.proc.remotesyslog.HeaderPart(hostname=hostname)
+        self.assertEqual(str(header), "%s %s" % (DEFAULT_TIMESTAMP, hostname))
 
 
 # check format of time and hostname, set automatically if incorrect
@@ -96,20 +87,128 @@ class HeaderPartTest(unittest.TestCase):
 # print message to stderr if badly formatted message encountered
 
 
+DEFAULT_TAG = "program"
+MOCK_PID = 1234
+
+
+class MockMsgTest(unittest.TestCase):
+
+    def mock_getpid(self):
+        return MOCK_PID
+
+    def setUp(self):
+        self.real_argv = sys.argv
+        sys.argv = [DEFAULT_TAG]
+        self.real_getpid = os.getpid
+        os.getpid = self.mock_getpid
+
+    def tearDown(self):
+        sys.argv = self.real_argv
+        os.getpid = self.real_getpid
+
+
+class MsgPartTest(MockMsgTest):
+
+    def test_tag_defaults_to_progname(self):
+        """Check TAG defaults to program name"""
+        msg = hacksaw.proc.remotesyslog.MsgPart()
+        self.assertEqual(msg.tag, DEFAULT_TAG)
+
+    def test_override_tag(self):
+        """Check TAG can be set manually to override program name"""
+        msg = hacksaw.proc.remotesyslog.MsgPart(tag="mytag")
+        self.assertEqual(msg.tag, "mytag")
+
+    def test_tag_trimmed_if_too_long(self):
+        """Check long TAGs are trimmed to 32 characters"""
+        tag = "abcd" * 10
+        msg = hacksaw.proc.remotesyslog.MsgPart(tag=tag)
+        self.assertEqual(msg.tag, tag[:32])
+
+    def test_space_prefixed_to_content(self):
+        """Check single space inserted infront of CONTENT if necessary"""
+        msg = hacksaw.proc.remotesyslog.MsgPart(content="hello")
+        self.assertEqual(str(msg), "%s: hello" % DEFAULT_TAG)
+
+    def test_space_only_added_if_necessary(self):
+        """Check space only added to CONTENT if necessary"""
+        msg = hacksaw.proc.remotesyslog.MsgPart(content=" hello")
+        self.assertEqual(str(msg), "%s hello" % DEFAULT_TAG)
+
+    def test_inclusion_of_pid(self):
+        """Check the program's pid can be included in CONTENT"""
+        msg = hacksaw.proc.remotesyslog.MsgPart(content="hello")
+        msg.include_pid()
+        self.assertEqual(str(msg), "%s[%d]: hello" % (DEFAULT_TAG, MOCK_PID))
+
+
+DEFAULT_PRI = hacksaw.proc.remotesyslog.PriPart(syslog.LOG_LOCAL4,
+                                                syslog.LOG_NOTICE)
+DEFAULT_HEADER = hacksaw.proc.remotesyslog.HeaderPart(DEFAULT_TIMESTAMP,
+                                                      DEFAULT_HOSTNAME)
+DEFAULT_MSG = hacksaw.proc.remotesyslog.MsgPart(DEFAULT_TAG, "hello")
+
+
 class SyslogMessageTest(unittest.TestCase):
 
     def test_message_format(self):
         """Check syslog message is correctly constructed"""
-        pri = hacksaw.proc.remotesyslog.PriPart(syslog.LOG_LOCAL4,
-                                                syslog.LOG_NOTICE)
-        header = hacksaw.proc.remotesyslog.HeaderPart(DEFAULT_TIMESTAMP,
-                                                      DEFAULT_HOSTNAME)
-        message = hacksaw.proc.remotesyslog.SyslogMessage(pri, header)
-        self.assertEqual(str(message), "<165>%s" % DEFAULT_HEADER)
-    
-# trailing space after hostname
-# maximum packet size is 1024 bytes
+        message = hacksaw.proc.remotesyslog.SyslogMessage(DEFAULT_PRI,
+                                                          DEFAULT_HEADER,
+                                                          DEFAULT_MSG)
+        header = " ".join((DEFAULT_TIMESTAMP, DEFAULT_HOSTNAME))
+        start_of_message = "<165>%s %s" % (header, DEFAULT_TAG)
+        self.assert_(str(message).startswith(start_of_message))
 
+    def test_max_lenth(self):
+        """Check that no syslog message is longer than 1024 bytes"""
+        message = "a" * 2048
+        message = hacksaw.proc.remotesyslog.SyslogMessage(DEFAULT_PRI,
+                                                          DEFAULT_HEADER,
+                                                          message)
+        self.assertEqual(len(str(message)),
+                         hacksaw.proc.remotesyslog.SyslogMessage.MAX_LEN)
+
+
+class LoggerTest(MockHeaderTest, MockMsgTest):
+
+    def setUp(self):
+        MockHeaderTest.setUp(self)
+        MockMsgTest.setUp(self)
+
+    def tearDown(self):
+        MockMsgTest.tearDown(self)
+        MockHeaderTest.tearDown(self)
+        
+    def test_send_message(self):
+        """Check we can send a simple message via UDP"""
+        hostname = "localhost"
+        message = hacksaw.proc.remotesyslog.SyslogMessage(DEFAULT_PRI,
+                                                          DEFAULT_HEADER,
+                                                          DEFAULT_MSG)
+        mock_sock = Mock()
+        port = hacksaw.proc.remotesyslog.Logger.PORT
+        mock_sock.expects(once()).send(eq(str(message)))
+        mock_sock.expects(once()).connect(eq((hostname, port)))
+
+        def mock_socket(family, proto):
+            return mock_sock
+        
+        real_socket = socket.socket
+        socket.socket = mock_socket
+        try:
+            logger = hacksaw.proc.remotesyslog.Logger()
+            logger.add_host("localhost")
+            logger.log(syslog.LOG_LOCAL4, syslog.LOG_NOTICE, "hello")
+            mock_sock.verify()
+        finally:
+            socket.socket = real_socket
+        
+
+# sending messages to multiple hosts
+# including the pid in the message
+# sending messages constructed by hand
+    
 
 if __name__ == "__main__":
     unittest.main()
