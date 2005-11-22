56a37435f738b29362ceff70db99143ff896139d
Graham Ashton <graham@effectif.com>
2005-11-22 16:33:50 +0000

    Implemented support for sending syslog messages via netsyslog.

--- hacksaw/src/hacksaw/proc/remotesyslog.py
+++ hacksaw/src/hacksaw/proc/remotesyslog.py
@@ -1,15 +1,106 @@
-# Copyright (C) 2005 Graham Ashton <ashtong@users.sourceforge.net>
 # $Id$
+# (C) Cmed Ltd, 2005
 
+
+import re
+import syslog
+
+import netsyslog
 
 import hacksaw.lib
 
 
+class LogMessage(object):
+
+    # This class was blatantly stolen from the Band Saw source.
+    # See http://bandsaw.sourceforge.net/.
+
+    regex = r"([^\s]+\s+[^\s]+\s+[^\s]+)\s+([^\s]+)\s+([^\s]+):\s(.*)"
+    pattern = re.compile(regex)
+    
+    def __init__(self, line):
+        self.match = LogMessage.pattern.match(line)
+
+    def _get_message_part(self, index):
+        try:
+            return self.match.groups()[index]
+        except AttributeError:
+            return ""
+
+    def _get_date(self):
+        return self._get_message_part(0)
+
+    date = property(_get_date)
+    
+    def _get_hostname(self):
+        return self._get_message_part(1)
+
+    hostname = property(_get_hostname)
+
+    def _get_process(self):
+        return self._get_message_part(2)
+
+    process = property(_get_process)
+
+    def _get_text(self):
+        return self._get_message_part(3)
+
+    text = property(_get_text)
+
+
 class Processor(hacksaw.lib.Processor):
 
-    pass
+    def split_process_info(self, text):
+        if "[" in text:
+            name, pid = text.split("[")
+            pid = int(pid[:-1])
+        else:
+            name = text
+            pid = None
+        return name, pid
+
+    def create_packet(self, message):
+        log = LogMessage(message)
+        pri = netsyslog.PriPart(self.config.facility, self.config.priority)
+        header = netsyslog.HeaderPart(log.date, log.hostname)
+        process_name, pid = self.split_process_info(log.process)
+        msg = netsyslog.MsgPart(process_name, log.text, pid)
+        return netsyslog.Packet(pri, header, msg)
+
+    def handle_message(self, message):
+        packet = self.create_packet(message)
+        logger = netsyslog.Logger()
+        for host in self.config.hosts:
+            logger.add_host(host)
+            logger.send_packet(packet)
 
 
 class Config(hacksaw.lib.Config):
 
-    pass
+    FACILITY = "facility"
+    HOSTS = "hosts"
+    PRIORITY = "priority"
+
+    def _get_facility(self):
+        name = self._get_item(Config.FACILITY)
+        constant = "LOG_" + name.upper()
+        return getattr(syslog, constant)
+
+    facility = property(_get_facility)
+
+    def _get_priority(self):
+        name = self._get_item(Config.PRIORITY)
+        if name == "warn":
+            name = "warning"
+        elif name == "error":
+            name = "err"
+        constant = "LOG_" + name.upper()
+        return getattr(syslog, constant)
+
+    priority = property(_get_priority)
+
+    def _get_hosts(self):
+        return [hostname.strip() for hostname in
+                self._get_item(Config.HOSTS).split(",")]
+
+    hosts = property(_get_hosts)
--- hacksaw/src/hacksaw/proc/remotesyslog_test.py
+++ hacksaw/src/hacksaw/proc/remotesyslog_test.py
@@ -1,10 +1,144 @@
-# Copyright (C) 2005 Graham Ashton <ashtong@users.sourceforge.net>
 # $Id$
+# (C) Cmed Ltd, 2005
 
 
+import syslog
 import unittest
 
+from pmock import *
+
+import hacksaw.lib_test
 import hacksaw.proc.remotesyslog
+
+
+class RemoteSyslogTest(hacksaw.lib_test.ConfigTest):
+
+    config_cls = hacksaw.proc.remotesyslog.Config
+
+    def setUp(self):
+        hacksaw.lib_test.ConfigTest.setUp(self)
+        self.append_to_file("[hacksaw.proc.remotesyslog]")
+
+
+class ConfigTest(RemoteSyslogTest):
+
+    def test_get_hosts(self):
+        """Check we can get list of hosts to which we send syslog packets"""
+        self.append_to_file("hosts: %s" % "localhost, otherhost")
+        self.assertEqual(self.config.hosts, ["localhost", "otherhost"])
+
+    def test_get_facility(self):
+        """Check we can get the syslog message facility"""
+        self.append_to_file("facility: local0")
+        self.assertEqual(self.config.facility, syslog.LOG_LOCAL0)
+
+    def test_get_priority(self):
+        """Check we can get the syslog message priority"""
+        self.append_to_file("priority: info")
+        self.assertEqual(self.config.priority, syslog.LOG_INFO)
+
+    def test_error_priority(self):
+        """Check priority 'error' converted to 'err'"""
+        self.append_to_file("priority: error")
+        self.assertEqual(self.config.priority, syslog.LOG_ERR)
+
+    def test_warning_priority(self):
+        """Check priority 'warn' converted to 'warning'"""
+        self.append_to_file("priority: warn")
+        self.assertEqual(self.config.priority, syslog.LOG_WARNING)
+
+
+class LogMessageTest(unittest.TestCase):
+
+    def setUp(self):
+        self.line = 'Jun 23 14:02:37 hoopoo ldap[29913]: Hello  world\n'
+
+    def test_date(self):
+        """Check we can extract the date from a log message"""
+        message = hacksaw.proc.remotesyslog.LogMessage(self.line)
+        self.assertEqual(message.date, 'Jun 23 14:02:37')
+
+    def test_date_single_figure_day(self):
+        """Check we can extract the date when the day is a single digit"""
+        line = 'Jun  1 14:02:37 hoopoo ldap[29913]: Hello  world\n'
+        message = hacksaw.proc.remotesyslog.LogMessage(line)
+        self.assertEqual(message.date, 'Jun  1 14:02:37')
+
+    def test_hostname(self):
+        """Check we can extract the hostname from a log message"""
+        message = hacksaw.proc.remotesyslog.LogMessage(self.line)
+        self.assertEqual(message.hostname, 'hoopoo')
+
+    def test_process(self):
+        """Check we can extract the process details from a log message"""
+        message = hacksaw.proc.remotesyslog.LogMessage(self.line)
+        self.assertEqual(message.process, 'ldap[29913]')
+        
+    def test_message(self):
+        """Check we can extract the text from a log message"""
+        message = hacksaw.proc.remotesyslog.LogMessage(self.line)
+        self.assertEqual(message.text, 'Hello  world')
+        
+
+class ProcessorTest(RemoteSyslogTest):
+
+    def test_get_process_only_name(self):
+        """Check we can get the process name when there is no pid"""
+        processor = hacksaw.proc.remotesyslog.Processor(self.config)
+        name, pid = processor.split_process_info("myproc")
+        self.assertEqual(name, "myproc")
+        self.assertEqual(pid, None)
+        
+    def test_get_process_name_and_pid(self):
+        """Check we can get the process name and pid when both set"""
+        processor = hacksaw.proc.remotesyslog.Processor(self.config)
+        name, pid = processor.split_process_info("myproc[123]")
+        self.assertEqual(name, "myproc")
+        self.assertEqual(pid, 123)
+
+    def test_messages_sent_to_hosts(self):
+        """Check syslog messages are sent to listed hosts"""
+        self.append_to_file("facility: daemon")
+        self.append_to_file("priority: warn")
+        self.append_to_file("hosts: localhost")
+        message = "Nov 22 08:59:54 myhost myproc[123]: Hello world!"
+
+        class MockModule(object):
+
+            pass
+
+        class MockClass(object):
+
+            def __call__(self, *args):
+                self.initargs = args
+                return self
+
+        mockmod = MockModule()
+        pri = MockClass()
+        header = MockClass()
+        msg = MockClass()
+        packet = MockClass()
+        
+        mockmod.PriPart = pri
+        mockmod.HeaderPart = header
+        mockmod.MsgPart = msg
+        mockmod.Packet = packet
+
+        try:
+            origmod = hacksaw.proc.remotesyslog.netsyslog
+            hacksaw.proc.remotesyslog.netsyslog = mockmod
+            
+            processor = hacksaw.proc.remotesyslog.Processor(self.config)
+            processor.create_packet(message)
+            
+            self.assertEqual(pri.initargs, (syslog.LOG_DAEMON,
+                                            syslog.LOG_WARNING))
+            self.assertEqual(header.initargs, ("Nov 22 08:59:54", "myhost"))
+            self.assertEqual(msg.initargs, ("myproc", "Hello world!", 123))
+            self.assertEqual(packet.initargs, (pri, header, msg))
+        finally:
+            hacksaw.proc.remotesyslog.netsyslog = origmod
+
 
 if __name__ == "__main__":
     unittest.main()
